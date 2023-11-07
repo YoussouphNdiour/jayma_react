b42742ca363de88623a934b9e0b2b519132f51db
YoussouphNdiour <83768182+YoussouphNdiour@users.noreply.github.com>
2023-11-07 12:32:18 +0000

    10 commit

---
+++ src/components/header/top-navbar/custom-language/languageLists.js
@@ -0,0 +1,30 @@
+import usFlag from "../../../../public/landingpage/us.svg";
+import arabicFlag from "../../../../public/landingpage/arabic-flag-svg.svg";
+import vietnamflag from "../../../../assets/country-flag/vn.svg";
+
+export const languageLists = [
+  {
+    languageName: "French",
+    languageCode: "fr",
+    countryCode: "SN",
+    countryFlag: usFlag.src,
+  },
+  {
+    languageName: "English",
+    languageCode: "en",
+    countryCode: "US",
+    countryFlag: usFlag.src,
+  },
+  {
+    languageName: "Arabic",
+    languageCode: "ar",
+    countryCode: "SA",
+    countryFlag: arabicFlag.src,
+  },
+  {
+    languageName: "Vietnamse",
+    languageCode: "vi",
+    countryCode: "VN",
+    countryFlag: vietnamflag.src,
+  },
+];
