696125f305128643622f22cd92b00858824f4fdc
YoussouphNdiour <83768182+YoussouphNdiour@users.noreply.github.com>
2023-12-19 03:55:40 +0000

    Update index.js

--- src/components/landing-page/index.js
+++ src/components/landing-page/index.js
@@ -1,44 +1,44 @@
 import React, {useEffect, useState} from "react";
-// import ComponentOne from "./ComponentOne";
-// import Registration from "./Registration";
-// import ComponentTwo from "./ComponentTwo";
+import ComponentOne from "./ComponentOne";
+import Registration from "./Registration";
+import ComponentTwo from "./ComponentTwo";
 import HeroSection from "./hero-section/HeroSection";
 import dynamic from "next/dynamic";
 import PushNotificationLayout from "../PushNotificationLayout";
-// import AppDownloadSection from "./app-download-section/index";
+import AppDownloadSection from "./app-download-section/index";
 import {useGeolocated} from "react-geolocated";
 import {useRouter} from "next/router";
-// import MapModal from "../Map/MapModal";
-// import Banners from "./Banners";
+import MapModal from "../Map/MapModal";
+import Banners from "./Banners";
 import {NoSsr, useMediaQuery, useTheme} from "@mui/material";
-// import DiscountBanner from "./DiscountBanner";
+import DiscountBanner from "./DiscountBanner";
 import CookiesConsent from "../CookiesConsent";
-import SplashScreen from "../../splash-screen";
+
 const LandingPage = ({configData, landingPageData}) => {
-    // const Testimonials = dynamic(() => import("./Testimonials"), {
-    //     ssr: false,
-    // });
-    const [location, setLocation] = useState("Dakar");
+    const Testimonials = dynamic(() => import("./Testimonials"), {
+        ssr: false,
+    });
+    const [location, setLocation] = useState(undefined);
     const [open, setOpen] = useState(false);
     const theme = useTheme();
-    // const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
-    // const {coords} = useGeolocated({
-    //     positionOptions: {
-    //         enableHighAccuracy: false,
-    //     }, userDecisionTimeout: 5000, isGeolocationEnabled: true,
-    // });
+    const isSmall = useMediaQuery(theme.breakpoints.down("sm"));
+    const {coords} = useGeolocated({
+        positionOptions: {
+            enableHighAccuracy: false,
+        }, userDecisionTimeout: 5000, isGeolocationEnabled: true,
+    });
     useEffect(() => {
         setLocation(JSON.stringify(localStorage.getItem("location")));
     }, []);
-    // const handleClose = () => {
-    //     const location = localStorage.getItem("location");
-    //     const isModuleExist = localStorage.getItem("module");
-    //     if (location) {
-    //         isModuleExist && setOpen(false);
-    //     } else {
-    //         setOpen(false);
-    //     }
-    // };
+    const handleClose = () => {
+        const location = localStorage.getItem("location");
+        const isModuleExist = localStorage.getItem("module");
+        if (location) {
+            isModuleExist && setOpen(false);
+        } else {
+            setOpen(false);
+        }
+    };
     const router = useRouter();
     const handleOrderNow = () => {
         if (location) {
@@ -58,7 +58,6 @@ const LandingPage = ({configData, landingPageData}) => {
                 landingPageData={landingPageData}
                 handleOrderNow={handleOrderNow}
             />
-            <SplashScreen/>
             {/* <ComponentOne
                 landingPageData={landingPageData}
                 configData={configData}
@@ -68,8 +67,8 @@ const LandingPage = ({configData, landingPageData}) => {
             <ComponentTwo
                 configData={configData}
                 landingPageData={landingPageData}
-            /> */}
-            {/* {(landingPageData?.earning_title || landingPageData?.earning_sub_title || landingPageData?.earning_seller_title || landingPageData?.earning_seller_sub_title || landingPageData?.earning_dm_title || landingPageData?.earning_dm_sub_title) && (
+            />
+            {(landingPageData?.earning_title || landingPageData?.earning_sub_title || landingPageData?.earning_seller_title || landingPageData?.earning_seller_sub_title || landingPageData?.earning_dm_title || landingPageData?.earning_dm_sub_title) && (
                 <Registration data={landingPageData} isSmall={isSmall}/>)}
             {
                 landingPageData?.fixed_promotional_banner && <DiscountBanner
@@ -81,7 +80,7 @@ const LandingPage = ({configData, landingPageData}) => {
                     configData={configData}
                     landingPageData={landingPageData}
                 />
-            }
+            } */}
             {landingPageData?.testimonial_list?.length > 0 &&
                 <Testimonials landingPageData={landingPageData} isSmall={isSmall}/>}
             {open && (<MapModal
@@ -89,9 +88,9 @@ const LandingPage = ({configData, landingPageData}) => {
                 handleClose={handleClose}
                 coords={coords}
                 disableAutoFocus
-            />)} */}
+            />)}
             <NoSsr>
-                <CookiesConsent text={"Nous utilisons des cookies et des technologies similaires sur notre site Web pour améliorer votre expérience de navigation et vous proposer un contenu personnalisé. En cliquant sur « Accepter » ou en continuant à utiliser notre site, vous acceptez l\'utilisation de ces cookies."}/>
+                <CookiesConsent text={configData?.cookies_text}/>
             </NoSsr>
 
         </PushNotificationLayout>
